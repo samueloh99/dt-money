ada747fd8dcd528d02341daa560b907e5954e168
samueloh99 <samueloh99@gmail.com>
2023-10-09 02:15:18 -0300

    Created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { Transactions } from "./pages/Transactions";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-
-      <h1>Hello World</h1>
+      <Transactions />
     </ThemeProvider>
   );
 }

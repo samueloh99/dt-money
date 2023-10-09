bc11db5b8c573ea28cdee575d22698b7fa6fa25d
samueloh99 <samueloh99@gmail.com>
2023-10-09 03:08:32 -0300

    Applying useMemo on hooks and search form component

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { MagnifyingGlass } from "phosphor-react";
 import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
@@ -12,7 +13,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => {
@@ -47,3 +48,5 @@ export function SearchForm() {
     </SearchFormContainer>
   );
 }
+
+export const SearchForm = memo(SearchFormComponent);
